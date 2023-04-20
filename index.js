27625bef6f465dc38ddd9630d6baed27f5717adf
pooranjoyb <pooranjoyb2016@gmail.com>
2023-04-20 17:16:06 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-import {Configuration, OpenAIApi} from "openai";
+import { Configuration, OpenAIApi } from "openai";
+import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv"
 
 dotenv.config();
 
 // Open AI Configuration
-const OpenAIconfig = new Configuration({
-    apiKey : process.env.API_KEY
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(OpenAIconfig);
+const openai = new OpenAIApi(configuration);
 
-async function runCompletion(message){
-    const completion = await openai.createCompletion({
-        model:"text-davinci-003",
-        max_tokens: 200,
-        prompt: message,
-    });
-    return completion.data.choices[0].text;
-}
+const bot = new TelegramBot(process.env.BOTKEY, { polling: true });
+
+bot.on('message', async (msg) => {
+    try{
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${msg.text}`,
+            temperature: 0.5,
+            frequency_penalty: 0.5,
+            max_tokens: 3000
+          });
+        // send a message to the chat acknowledging receipt of their message
+        bot.sendMessage(msg.chat.id, response.data.choices[0].text);
+    }catch(error){
+        bot.sendMessage(msg.chat.id, error.message);
+    }
+
+});
